c055f9a10d26ae922bfe69fce838d8f1902f3fe8
菅沼 洋平 <yohei@suganumanoMacBook-Air.local>
2024-10-01 16:21:07 +0900

    1st

---
+++ src/animal.py
@@ -0,0 +1,26 @@
+from torchvision import transforms
+import pytorch_lightning as pl
+import torch.nn as nn
+
+# from torchvision.models import resnet18
+from torchvision.models import mnasnet1_0
+
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+class Net(pl.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+
+        self.feature = mnasnet1_0(pretrained=True)
+        self.fc = nn.Linear(1000, 2)
+
+    def forward(self, x):
+        h = self.feature(x)
+        h = self.fc(h)
+        return h
